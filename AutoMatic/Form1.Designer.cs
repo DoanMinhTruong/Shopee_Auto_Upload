fcb86d3b330f485d4397f8e8f84df531e1d5795b
unknown <meir.truong.2309@gmail.com>
2020-09-15 23:45:40 +0700

    CsharpVersion1

---
+++ AutoMatic/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace AutoMatic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.lstFolder = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(390, 360);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(197, 46);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Enabled = false;
+            this.txtFolder.Location = new System.Drawing.Point(90, 11);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(497, 20);
+            this.txtFolder.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Location Folder";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LOAD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstFolder
+            // 
+            this.lstFolder.FormattingEnabled = true;
+            this.lstFolder.Location = new System.Drawing.Point(7, 95);
+            this.lstFolder.Name = "lstFolder";
+            this.lstFolder.Size = new System.Drawing.Size(580, 251);
+            this.lstFolder.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 373);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Bảng Size";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Add ProFile FireFox : ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(248, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(339, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 417);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstFolder);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "AUTOMATIC SHOPEE - EzsKey";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ListBox lstFolder;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ AutoMatic/Form1.cs
@@ -0,0 +1,271 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Support.Events;
+using System.CodeDom.Compiler;
+using System.Windows.Forms.VisualStyles;
+
+namespace AutoMatic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstFolder.Text = "";
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            string f = folderBrowserDialog1.SelectedPath;
+            ////////Path Đường Dẫn 
+            string[] len_F = f.Split('\\');
+            /////////////////////////
+            
+                txtFolder.Text = f;
+                string[] arr = Directory.GetFiles(f, "*.jpg");
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+
+                    string[] it = arr[i].Split('\\');
+                    string[] it2 = it[len_F.Length].Split(' ');
+                    string ok = "";
+                    for(int q = 1; q < it2.Length;q++)
+                    {
+                        ok += " " + it2[q];
+                    }
+                    ok = ok.Replace(".jpg", "");
+                    lstFolder.Items.Add(ok);
+                }
+            
+        }
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string mota = @"
++Size S: từ 37-47kg
++Size M: từ 48-57kg
++Size L: từ 58-68kg
++Size XL: từ 69-77kg
++Size XXL: từ 78-88kg
+*Các bạn muốn mang ôm body hoặc rộng vui lòng chọn chệch đi 1 size hoặc inbox chúng mình để được tư vấn nha ^^.
+    
+✈ Form rộng Unisex cho cả nam và nữ 
+Nhận THU HỘ ( ship COD ) toàn quốc 
+
+✅Hình thật giống mẫu _ Đảm bảo chất lượng hơn giá tiền bạn bỏ ra .
+✅Áo được kiểm tra kỹ càng, cẩn thận và tư vấn nhiệt tình trước khi giao hàng cho Khách
+💥Hoàn lại tiền 💯% nếu hàng “ Lỗi Kỹ thuật  - Sai Mẫu - Màu “ 
+
+#aothuntaylo #aothun #aothunnu #aocouple #aothunngantay #aothuncotron #aothunnam #aothunthietke #aodesign
+            ";
+                FirefoxProfile profile = new FirefoxProfileManager().GetProfile(comboBox1.SelectedItem.ToString());
+                FirefoxOptions options = new FirefoxOptions();
+                options.Profile = profile;
+                FirefoxDriver fd = new FirefoxDriver(options);
+
+                string f = folderBrowserDialog1.SelectedPath;
+                string[] arr = Directory.GetFiles(f, "*.jpg");
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    fd.Navigate().GoToUrl("https://banhang.shopee.vn/portal/product/category/");
+
+
+                    //////
+                    Thread.Sleep(2000);
+                    fd.FindElementByTagName("html").Click();
+                    SendKeys.SendWait(@"^{SUBTRACT}");
+                    SendKeys.SendWait(@"^{SUBTRACT}");
+                    SendKeys.SendWait(@"^{SUBTRACT}");
+                    SendKeys.SendWait(@"^{SUBTRACT}");
+
+                    ////////////
+                    Thread.Sleep(1000);
+                    fd.FindElement(By.XPath("//p[@class='text-overflow' and text()='Thời Trang Nam']")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("//p[@class='text-overflow' and text()='Áo thun']")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("//p[@class='text-overflow' and text()='Áo ngắn tay không cổ']")).Click();
+                    Thread.Sleep(200);
+                    var ten_SP = fd.FindElement(By.XPath("//input[@type='text' and @class='shopee-input__input' and @placeholder='Nhập vào']"));
+                    ten_SP.SendKeys("Áo Thun Tay Lỡ Nam Nữ ");
+
+
+                    string[] b = arr[i].Split('\\');
+                    string c = b[b.Length - 1];
+                    string[] cc = c.Split(' ');
+                    string d = "";
+                    for (int k = 1; k < cc.Length; k++)
+                    {
+                        d += " " + cc[k];
+                    }
+                    string mm = "";
+                    for (int k = 0; k < d.Length - 4; k++)
+                    {
+                        mm += d[k];
+                    }
+                    ten_SP.SendKeys(mm);
+                    Thread.Sleep(500);
+
+
+                    fd.FindElement(By.XPath("//button[@type='button' and @class='shopee-button shopee-button--primary shopee-button--large']")).Click();
+                    Thread.Sleep(2000);
+                    /////////
+
+                    ////////////////
+
+
+
+                    /////////////////////////////////////////////////////////
+                    ///
+
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[1]/div/div[2]/div/div[1]/div[2]/div/div/div/div[1]")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[1]/div/div[2]/div/div[1]/div[2]/div/div/div/div[2]/ul/div[2]/li[1]")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[1]/div/div[2]/div/div[2]/div[2]/div/div/div/div[1]")).Click();
+
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[1]/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/ul/div[2]/li[7]")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[1]/div/div[2]/div/div[3]/div[2]/div/div/div/div[1]")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[1]/div/div[2]/div/div[3]/div[2]/div/div/div/div[2]/ul/div[2]/li[1]")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("//textarea[@type='textarea' and @class='shopee-input__inner shopee-input__inner--normal']")).SendKeys(mota);
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[3]/div[1]/ul/li[2]/a")).Click();
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[3]/div[2]/button")).Click();
+                    Thread.Sleep(1500);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[2]/div/div/div[1]/div/input")).SendKeys("Màu Sắc");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[1]/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/input")).SendKeys("Đen");
+                    Thread.Sleep(500);
+                    ///
+                    /// 
+                    /// 
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/button")).Click();
+                    Thread.Sleep(1500);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[2]/button")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[2]/button")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[2]/button")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[2]/button")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[3]/div[2]/button")).Click();
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[2]/div/div/div[1]/div/input")).SendKeys("Sizes");
+
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[1]/div[2]/div/div/div[1]/div/input")).SendKeys("S");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/div[2]/div/div/div[1]/div/input")).SendKeys("M");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div[2]/div/div/div[1]/div/input")).SendKeys("L");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[4]/div[2]/div/div/div[1]/div/input")).SendKeys("XL");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[5]/div[2]/div/div/div[1]/div/input")).SendKeys("XXL");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[6]/div[2]/div/div/div[1]/div/input")).SendKeys("XXXL");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[1]/div[2]/div/div/div/div[1]/div/input")).SendKeys("109000");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div[1]/div/input")).SendKeys("109000");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[3]/div[2]/div/div/div/div[1]/div/input")).SendKeys("109000");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[4]/div[2]/div/div/div/div[1]/div/input")).SendKeys("109000");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[5]/div[2]/div/div/div/div[1]/div/input")).SendKeys("109000");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[6]/div[2]/div/div/div/div[1]/div/input")).SendKeys("109000");
+                    ///////
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[1]/div[3]/div/div/div/div/div/div/input")).SendKeys("50");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[2]/div[3]/div/div/div/div/div/div/input")).SendKeys("50");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[3]/div[3]/div/div/div/div/div/div/input")).SendKeys("50");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[4]/div[3]/div/div/div/div/div/div/input")).SendKeys("50");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[5]/div[3]/div/div/div/div/div/div/input")).SendKeys("50");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[2]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[2]/div[6]/div[3]/div/div/div/div/div/div/input")).SendKeys("50");
+                    ///////////////////////////
+                    fd.FindElementByXPath("/html/body/div[1]/div[2]/div/div/div/div/div[3]/div[1]/ul/li[3]/a").Click();
+                    Thread.Sleep(200);
+
+
+                   
+
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[3]/div/div[1]/div[2]/div[1]/div[1]/div/div[1]/div/div/div")).Click();
+                    Thread.Sleep(200);
+                    SendKeys.Send(System.Text.RegularExpressions.Regex.Replace(arr[i]+ @"{Enter}", @"[()‘’,.#!%'""//\\:&~`+-=_]", "{$0}"));
+
+
+                    //SendKeys.SendWait("{Enter}");
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[3]/div/div[3]/div/div[2]/div[1]/div/div/div")).Click();
+                    Thread.Sleep(200);
+                    SendKeys.Send(System.Text.RegularExpressions.Regex.Replace(textBox1.Text+ @"{Enter}", @"[:\\]", "{$0}"));
+
+                    //SendKeys.SendWait("{Enter}");
+
+                    Thread.Sleep(200);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[4]/div/div[1]/div[1]/div[2]/div/div/div[1]/div/input")).SendKeys("300");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[4]/div/div[1]/div[2]/div[2]/div/div[1]/div/div/div/input")).SendKeys("30");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[4]/div/div[1]/div[2]/div[2]/div/div[2]/div/div/div/input")).SendKeys("40");
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/section[4]/div/div[1]/div[2]/div[2]/div/div[3]/div/div/div/input")).SendKeys("10");
+                    SendKeys.SendWait(@"^{ADD}");
+                    SendKeys.SendWait(@"^{ADD}");
+                    SendKeys.SendWait(@"^{ADD}");
+                    SendKeys.SendWait(@"^{ADD}");
+                    Thread.Sleep(1000);
+                    fd.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[2]/div[1]/div/div/button[3]")).Click();
+                    Thread.Sleep(5000);
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Kiểm tra lại đầy đủ input");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult s = openFileDialog1.ShowDialog();
+            string result = openFileDialog1.FileName;
+            textBox1.Text = result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string add = @"C:\Users\DM-TRUONG\AppData\Roaming\Mozilla\Firefox\Profiles";
+            string[] profiles = Directory.GetDirectories(add);
+            
+            foreach (var i in profiles)
+            {
+                string[] ii = i.Split('\\');
+                string[] iii = ii.ElementAt(ii.Length-1).Split('.');
+                comboBox1.Items.Add(iii.ElementAt(iii.Length-1));
+               
+            }    
+
+
+        }
+    }
+}
